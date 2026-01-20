ae95a0c3d8e4ed25373f20d09038fe589201dc11
watillas <watillasmateus554@gmail.com>
2026-01-20 19:58:11 -0300

    arrumando os seguidores , e usando o try e cath

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const ProfileAvatar = document.querySelector(".profile-avatar");
     const ProfileName = document.querySelector(".profile-name");
     const ProfileUserName = document.querySelector(".profile-username");
-    const NumberRepos = document.querySelector(".number-repos");
-    const NumberFollowers = document.querySelector(".number-followers");
-    const NumberFollowing = document.querySelector(".number-following");
+    const NumberRepos = document.querySelector(".repositories");
+    const NumberFollowers = document.querySelector(".followers");
+    const NumberFollowing = document.querySelector(".following");
     const ProfileLink = document.querySelector(".profile-link");
 
-    fetch('https://api.github.com/users/Watillas')
+    try {
+        fetch('https://api.github.com/users/Watillas')
     .then(function(response) {
         return response.json();
     })
@@ -19,10 +20,10 @@ document.addEventListener("DOMContentLoaded", function() {
         NumberFollowers.textContent = json.followers;
         NumberFollowing.textContent = json.following;
         ProfileLink.href = json.html_url;
-    })
-
-
-
+    });
+    } catch (error) {
+        console.error('Erro ao buscar dados do GitHub:', error);
+    }
 });
 
 
